74060e3260d2303850549509cb2a3e2186cfe0b4
myOmikron <accounts@omikron.pw>
2020-08-04 04:06:31 +0200

    Added urls

--- planetside_scrim_manager/planetside_scrim_manager/settings.py
+++ planetside_scrim_manager/planetside_scrim_manager/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scoreboard',
+    'admin',
+    'teams',
+    'dashboard'
 ]
 
 MIDDLEWARE = [
