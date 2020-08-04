9e5e9d291c980381d12ea0c12363ea4ebe996f23
myOmikron <accounts@omikron.pw>
2020-08-04 04:59:21 +0200

    Added new IndexView at ''

--- planetside_scrim_manager/dashboard/views.py
+++ planetside_scrim_manager/dashboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+
+class IndexView(TemplateView):
+    template_name = "dashboard/index.html"
+
+    def get(self, request, *args, **kwargs):
+        return render(request, template_name=self.template_name)
