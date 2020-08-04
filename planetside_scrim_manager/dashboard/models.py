0a80b1aa833ec797034250aa960731b1163a8adb
myOmikron <accounts@omikron.pw>
2020-08-04 05:08:15 +0200

    Added Models Team and Player

--- planetside_scrim_manager/dashboard/models.py
+++ planetside_scrim_manager/dashboard/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.db.models import CharField, ForeignKey
 
-# Create your models here.
+
+class Team(models.Model):
+    team_name = CharField(default="", blank=False, max_length=255)
+
+
+class Player(models.Model):
+    player_name = CharField(default="", blank=False, max_length=255)
+    team = ForeignKey(Team, on_delete=models.CASCADE)
